341f1baeff80e6581492f8061a6d21321baeea26
madhavikiran <madhavikiran4@gmail.com>
2025-08-11 22:05:52 +0530

    updated

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const http = require('http');
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js v22!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
